0f3a60c0089b57b4ae276379bd8c8c5665ed7738
Joost van den Brandt <jaabrand@pc131119.local>
2015-03-05 10:41:42 +0100

    Initial Commit

---
+++ SpotApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SpotApp
+//
+//  Created by Joost van den Brandt on 05/03/15.
+//  Copyright (c) 2015 Joost van den Brandt. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
